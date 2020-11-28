6acdeb6171c6656de22fb0b14e99a651948c219f
Fabian Streicher <fabi.streicher@gmail.com>
2020-11-28 13:58:58 +0100

    chore: disable no-shadow rule

--- functions/src/utils.const.ts
+++ functions/src/utils.const.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 export enum FirebaseRegions {
   BELGIUM = 'europe-west1',   // Tier 1 pricing $
   LONDON = 'europe-west2',    // Tier 1 pricing $
@@ -5,13 +6,10 @@ export enum FirebaseRegions {
   ZURICH = 'europe-west6'     // Tier 2 pricing $$$
 }
 
-export enum HttpMethod {
+export enum HTTPMethod {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
   PATCH = 'PATCH',
   DELETE = 'DELETE'
 }
-
-export const UUID_NAME = 'GTA ScavengerHunt Images';
-export const UUID_NAMESPACE = '104e354d-5078-4abe-bbc0-1c8a38fc9370';
