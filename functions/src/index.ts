f5b40c7002d6e0a0c45ee025ac0764035969dad3
Fabian Streicher <fabi.streicher@gmail.com>
2022-04-09 12:51:49 +0200

    chore(deps): upgrade deps

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -47,30 +47,7 @@ export const convertToWebp = functions
     const filename = `${v4()}.webp`;
     const file = new Storage().bucket('gta-dashboard.appspot.com').file(`${DIR}/${filename}`);
 
-    const originalImage = sharp(req.body);
-    const metadata = await originalImage.metadata();
-    const mapHeight = Math.floor(metadata.height / 4.8);
-
-    // crop minimap and blur
-    const miniMapSingle = await sharp(req.body)
-      .extract({
-        top: metadata.height - mapHeight,
-        left: 0,
-        height: mapHeight,
-        width: Math.floor(mapHeight * 3 / 2)
-      })
-      .blur(30)
-      .webp()
-      .toBuffer()
-
-    // composite blurred minimap onto original image
-    originalImage
-      .composite([{
-        input: miniMapSingle,
-        gravity: 'southwest'
-      }])
-      .webp()
-      .toBuffer()
+    convertImage(req.body)
       .then(buffer => {
         // upload buffer to bucket
         file.save(buffer, { resumable: false, metadata: { cacheControl: 'public, max-age=2562000' } })
@@ -82,7 +59,56 @@ export const convertToWebp = functions
       .catch(err => {
         res.status(500).send(err);
       });
+  });
 
+export const convertAndBlur = functions
+  .region(FirebaseRegions.FRANKFURT)
+  .runWith(options)
+  .https
+  .onRequest(async (req, res) => {
+    // allow CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    functions.logger.debug(req.method, req.headers);
+
+    // handle preflight requests
+    if (req.method === HTTPMethod.OPTIONS) {
+      res.status(200).send();
+      return;
+    }
+
+    // deny anything but POST requests
+    if (req.method !== HTTPMethod.POST) {
+      res.status(405).json({ message: 'This endpoint only accepts POST requests' });
+      return;
+    }
+
+    // only accept images
+    if (!req.headers?.['content-type'] || (req.headers?.['content-type'] !== 'image/png' && req.headers?.['content-type'] !== 'image/jpeg')) {
+      res.status(415).json({ message: 'This endpoint only accepts content types \'image/png\' and \'image/jpeg\'' });
+      return;
+    }
+
+    functions.logger.debug('Checks passed, continue processing...');
+
+    // create storage file handler
+    const filename = `${v4()}.webp`;
+    const file = new Storage().bucket('gta-dashboard.appspot.com').file(`${DIR}/${filename}`);
+
+    convertImage(req.body, true)
+      .then(buffer => {
+        // upload buffer to bucket
+        file.save(buffer, { resumable: false, metadata: { cacheControl: 'public, max-age=2562000' } })
+          .then(_ => {
+            res.status(201).send({ filename: filename });
+          })
+          .catch(err => res.status(500).send(err));
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
   });
 
 export const deleteImage = functions
@@ -127,3 +153,34 @@ export const deleteImage = functions
       });
 
   });
+
+
+async function convertImage(image: any, blurFlag: boolean = false): Promise<Buffer> {
+
+  const originalImage = sharp(image);
+  const metadata = await originalImage.metadata();
+  const mapHeight = Math.floor(metadata.height / 4.8);
+  const compositeLayers: Array<sharp.OverlayOptions> = [];
+
+  if (blurFlag) {
+    // crop minimap and blur
+    const miniMapSingle = await sharp(image)
+      .extract({
+        top: metadata.height - mapHeight,
+        left: 0,
+        height: mapHeight,
+        width: Math.floor(mapHeight * 3 / 2)
+      })
+      .blur(30)
+      .webp()
+      .toBuffer();
+
+    compositeLayers.push({ input: miniMapSingle, gravity: 'southwest' });
+  }
+
+  // composite blurred minimap onto original image
+  return originalImage
+    .composite(compositeLayers)
+    .webp()
+    .toBuffer();
+}
